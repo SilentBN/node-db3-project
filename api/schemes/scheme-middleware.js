09c2e8d086de240eafa313b82475fdfa7bc3b219
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-07-05 16:29:19 -0400

    Implement middleware functions for scheme validation

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const { findById } = require("./scheme-model");
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -7,8 +9,20 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
-}
+  const { scheme_id } = req.params;
+  findById(scheme_id)
+    .then((scheme) => {
+      if (scheme) {
+        req.scheme = scheme;
+        next();
+      } else {
+        res
+          .status(404)
+          .json({ message: `scheme with scheme_id ${scheme_id} not found` });
+      }
+    })
+    .catch(next);
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,8 +33,13 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  const { scheme_name } = req.body;
+  if (!scheme_name || typeof scheme_name !== "string" || !scheme_name.trim()) {
+    res.status(400).json({ message: "invalid scheme_name" });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,11 +51,23 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
-}
+  const { instructions, step_number } = req.body;
+  if (
+    !instructions ||
+    typeof instructions !== "string" ||
+    !instructions.trim() ||
+    !step_number ||
+    typeof step_number !== "number" ||
+    step_number < 1
+  ) {
+    res.status(400).json({ message: "invalid step" });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
